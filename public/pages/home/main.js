15aef2caf5803c0ab0a1d31c1c4d6aa6524a11b1
N. Monalisa <nathaliamonalisa@gmail.com>
2020-06-03 14:39:43 -0300

    refatoração do template da page login

--- public/pages/home/main.js
+++ public/pages/home/main.js
@@ -1,83 +1,64 @@
 import { example } from './data.js';
 
-const createElements = () => {
-  // container da página
-  const main = document.querySelector('#root');
+// Variáveis globais:
+const main = document.querySelector('#root');
 
-  // criar container geral do login
-  const generalContainer = document.createElement('div');
+const createTags = (tag) => document.createElement(tag);
+const appendTags = (parentNode, childNode) => parentNode.appendChild(childNode);
 
-  // anexar container ao root
-  main.appendChild(generalContainer);
-  generalContainer.style.backgroundColor = 'silver';
 
-  // criar container do logotipo
-  const logotypeContainer = document.createElement('div');
+// Criar elementos da página:
+const createElementsPage = () => {
+  // lista de tags utilizadas
+  const tagsTypes = ['div', 'img', 'p', 'form', 'input', 'button'];
 
-  // img da logo
-  const logotypeIcon = document.createElement('img');
-  logotypeIcon.src = '';
-  logotypeIcon.style.width = '25px';
-  logotypeIcon.style.height = '25px';
-  logotypeIcon.style.display = 'block';
+  const pageContainer = createTags(tagsTypes[0]);
+  const logotypeContainer = createTags(tagsTypes[0]);
+  const btnAreaContainer = createTags(tagsTypes[0]);
+  const logotypeIcon = createTags(tagsTypes[1]);
+  const googleIcon = createTags(tagsTypes[1]);
+  const registerText = createTags(tagsTypes[2]);
+  const logotypeText = createTags(tagsTypes[2]);
+  const form = createTags(tagsTypes[3]);
+  const emailInput = createTags(tagsTypes[4]);
+  const passwordInput = createTags(tagsTypes[4]);
+  const loginBtn = createTags(tagsTypes[5]);
+  const googleBtn = createTags(tagsTypes[5]);
 
-  // texto da logo
-  const logotypeText = document.createElement('div');
-  logotypeText.textContent = 'Seja bem-vinde!';
+  appendTags(main, pageContainer);
+  // Elementos da logo
+  appendTags(logotypeContainer, logotypeIcon);
+  appendTags(logotypeContainer, logotypeText);
+  appendTags(pageContainer, logotypeContainer);
+  // Elementos do form
+  appendTags(form, emailInput);
+  appendTags(form, passwordInput);
+  appendTags(pageContainer, form);
+  // Botões
+  appendTags(btnAreaContainer, loginBtn);
+  appendTags(btnAreaContainer, googleBtn);
+  appendTags(btnAreaContainer, googleIcon);
+  appendTags(btnAreaContainer, registerText);
+  appendTags(pageContainer, btnAreaContainer);
+};
 
-  // anexar container da logo
-  logotypeContainer.appendChild(logotypeIcon);
-  logotypeContainer.appendChild(logotypeText);
-  generalContainer.appendChild(logotypeContainer);
 
-  // criar form
-  const form = document.createElement('form');
+const setStyleOnElements = () => {
+  document.getElementsByTagName('p')[0].textContent = 'Seja bem-vinde!';
+  document.getElementsByTagName('p')[1].innerHTML = `Se não tem uma conta, <a href="
+  https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp"> registre-se! </a>`;
+  document.getElementsByTagName('input')[0].placeholder = 'E-mail';
+  document.getElementsByTagName('input')[1].placeholder = 'Senha';
+  document.getElementsByTagName('button')[0].innerHTML = 'Entrar';
+  document.getElementsByTagName('button')[1].innerHTML = 'Acesse com';
 
-  // criar inputs
-  const emailInput = document.createElement('input');
-  const passwordInput = document.createElement('input');
 
-  emailInput.placeholder = 'E-mail';
-  passwordInput.placeholder = 'Senha';
-
-  form.appendChild(emailInput);
-  form.appendChild(passwordInput);
-
-  generalContainer.appendChild(form);
-
-  // área de botões
-  const btnAreaContainer = document.createElement('div');
-  const loginBtn = document.createElement('button');
-  const googleBtn = document.createElement('button');
-  const googleIcon = document.createElement('img');
-  const registerText = document.createElement('p');
-  registerText.innerHTML = `Se não tem uma conta, <a href="
-https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp"> registre-se! </a>`;
-
-  // Propriedade dos botões
-  loginBtn.innerHTML = 'Entrar';
-  googleBtn.innerHTML = 'Acesse com';
-  googleIcon.src = '';
-  googleIcon.style.width = '25px';
-  googleIcon.style.height = '25px';
-  googleIcon.style.display = 'block';
-
-  // anexar botões
-  btnAreaContainer.appendChild(loginBtn);
-  btnAreaContainer.appendChild(googleBtn);
-  btnAreaContainer.appendChild(googleIcon);
-  btnAreaContainer.appendChild(registerText);
-  generalContainer.appendChild(btnAreaContainer);
 };
 
 const home = () => {
   // Executar funções
-  createElements();
-
-  const setStylesOnElements = () => {
-    // declaração;
-    // colocar o ID e as classes nos elementos
-  };
+  createElementsPage();
+  setStyleOnElements();
 };
 
 export { home };
