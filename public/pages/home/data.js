4671fe293a24c42ce1424c8899fc1e78b297379d
renataalmeids <ras_almeida@hotmail.com>
2020-06-05 17:46:42 -0300

    tentativa login

--- public/pages/home/data.js
+++ public/pages/home/data.js
@@ -1,1 +1,9 @@
 export const example = page => `Eu estou na página ${page}`;
+export const registro = (email, senha) => {
+firebase.auth().createUserWithEmailAndPassword(email, senha).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    });
+}
--- public/pages/home/main.js
+++ public/pages/home/main.js
@@ -1,6 +1,27 @@
 // Aqui serão criados os eventos de Manipulação de DOM e templates
-import { example } from './data.js';
+import { registro } from './data.js';
+
 
 export const home = () => {
-    document.querySelector('#root').textContent = example('Home');
+  const container = document.createElement('div');
+
+  container.innerHTML = `
+    <form>
+      <input id='email' type='text'>
+      <input id='senha' type='text'>
+      <button id='registro'>registre-se</button>
+    </form>
+    <div id='greeting-message'></div>
+  `;
+
+  const email = container.querySelector('#email');
+  const senha = container.querySelector('#senha');
+  const registroBtn = container.querySelector('#registro');
+
+  registroBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    registro (email.value, senha.value)
+  });
+
+  return container;
 };
