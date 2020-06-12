94e8497c47ba5916265b4ba0552647ea697ca8f6
Thalita Gonçalves <thagsousa15@gmail.com>
2020-06-11 21:08:14 -0300

    Correção do bug de login com o google

--- public/pages/home/data.js
+++ public/pages/home/data.js
@@ -14,14 +14,22 @@ export const signIn = (email, password) => {
 
 export const loginWithGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider).then((result) => {
-    const token = result.credential.accessToken;
-    const user = result.user;
-    window.location.hash = '#generalFeed';
-  }).catch(error => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    const email = error.email;
-    const credential = error.credential;
+  firebase.auth().signInWithPopup(provider)
+  .then((result) => {
+    window.location.hash = '#generalFeed'
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
   });
-};
+}
