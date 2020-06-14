dcde541a0157a073eae7aa08ef9fca73fa054c8d
renataalmeids <ras_almeida@hotmail.com>
2020-06-14 17:25:56 -0300

    testando nova tratativa de erro login

--- public/pages/home/data.js
+++ public/pages/home/data.js
@@ -1,32 +1,14 @@
-<<<<<<< HEAD
 import { generalFeed } from '../generalFeed/main.js';
 
-
-export const signIn = (email, password) => {
-  return firebase
-=======
 export const signIn = (email, password, mexirica) => {
   firebase
->>>>>>> 441ce91e7147d42b425ad2a3dff8b837849bef3e
     .auth()
     .signInWithEmailAndPassword(email, password)
     .then((result) => {
       window.location.hash = '#generalFeed';
       return result;
     })
-<<<<<<< HEAD
-    .catch((error) => {
-      const errorCode = error.code;
-      console.log(error)  
-      if(errorCode ==='auth/user-not-found'){
-        return 'usuário não encontrado'
-        }else if(errorCode === 'auth/wrong-password'){
-          return 'senha inválida'
-      }
-    });
-=======
     .catch(error => mexirica(error));
->>>>>>> 441ce91e7147d42b425ad2a3dff8b837849bef3e
 };
 
 export const loginWithGoogle = () => {
