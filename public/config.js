ae46c75a69a1ef92827c669ca9073e064c6580d7
renataalmeids <ras_almeida@hotmail.com>
2020-06-23 16:20:23 -0300

    exclusão de observaações

---
+++ public/config.js
@@ -0,0 +1,17 @@
+  // Your web app's Firebase configuration
+    var firebaseConfig = {
+    apiKey: "AIzaSyDu57QUgGeSYkeJFtPncWWX4bU3oULUoX8",
+    authDomain: "social-network-c1b66.firebaseapp.com",
+    databaseURL: "https://social-network-c1b66.firebaseio.com",
+    projectId: "social-network-c1b66",
+    storageBucket: "social-network-c1b66.appspot.com",
+    messagingSenderId: "624612323786",
+    appId: "1:624612323786:web:52c4b4ca1fcfdb8231df33",
+    measurementId: "G-X1LBFV0ME5"
+    };
+  // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    //firebase.analytics();
+
+
+
