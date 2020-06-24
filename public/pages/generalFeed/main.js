9eb2eabd4ae556aee8f84cacaeebe974a623318d
N. Monalisa <nathaliamonalisa@gmail.com>
2020-06-24 16:12:48 -0300

    Inseri mensagem de carregamento de fotos enquanto o usuário aguarda o upload

--- public/pages/generalFeed/main.js
+++ public/pages/generalFeed/main.js
@@ -57,11 +57,17 @@ const sendNewProfileImg = (callbackToSetNewImage) => {
 
 const showUrlOfImagesToPublish = (urlFile) => {
   document.querySelector('#postText').value = `${urlFile}`;
+  document.querySelector('#postText').placeholder = 'O que você quer compartilhar?';
+
 };
 
 const uploadImage = () => {
   document.querySelector('.publish-img-form-box').style.opacity = 1;
-  document.querySelector('#image_uploads').onchange = event => sendImageToDatabase(event.target.files[0], showUrlOfImagesToPublish);
+  document.querySelector('#image_uploads').onchange = event => {
+    sendImageToDatabase(event.target.files[0], showUrlOfImagesToPublish)
+    document.querySelector('.publish-img-form-box').style.opacity = 0;
+    document.querySelector('#postText').placeholder = 'Aguarde enquanto sua foto é carregada...';
+  };
 };
 
 const getUpLoadImgClick = () => document.querySelector('#publish-img-btn').addEventListener('click', uploadImage);
