602a48ed7cdf672c53220a44833689566e3ddaa7
Thalita Gonçalves <thagsousa15@gmail.com>
2020-06-23 23:20:49 -0300

    Funcionalidade de comentários pronta

--- public/pages/generalFeed/main.js
+++ public/pages/generalFeed/main.js
@@ -119,17 +119,19 @@ const loadPostTemplate = (postList) => {
   <footer class='footer-post-box'>
      <div><img class='post-area-icon' id="like-icon" src="../../assets/like.png" alt="Like Icon"></div>
     <div class='post-area-icon' id='likes-counter'>${likes.length}</div>  
-    <div><img class='post-area-icon' src="../../assets/comments.png" alt="Comments Icon">${comments}</div>
-    ${comments.forEach(({ name, textComment }) => `<div>
-      <p>${name}</p>
-      <p>${textComment}</p>
-      </div>`)}
+    <div><img class='post-area-icon' src="../../assets/comments.png" alt="Comments Icon">${comments.length}</div>
+    ${comments.length > 0 && comments.map(comment => `
+    <div>
+     <p>${comment.name}</p>
+     <p>${comment.text}</p>
+     </div>
+     `)}
     <textarea id="text-comment"></textarea>
     <button id="send-comment">Comentar</button>
     <div class='edit-btn'><img class='post-area-icon' src="../../assets/pencil.png" alt="Edit Icon"></div>
   </footer>
   `;
-    console.log(comments);
+
     postBox.querySelector('#send-comment').addEventListener('click', () => commentPosts(code, postBox.querySelector('#text-comment').value));
     postBox.querySelector('#like-icon').addEventListener('click', () => likePosts(code));
 
